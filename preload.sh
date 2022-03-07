f8e20e92afd60435532619ad09fdd301313e680b
aobiomi <91564146+aobiomi@users.noreply.github.com>
2022-03-07 21:19:02 +0300

    Update preload.sh

--- preload.sh
+++ preload.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install jq screen curl -y
 cd ~
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz > /dev/null
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz -q
 tar -xf ngrok-stable-linux-amd64.tgz
 
 ./ngrok authtoken $NGROK_AUTH_TOKEN
@@ -15,4 +15,4 @@ sudo usermod -aG sudo bidgido
 
 echo IP: $(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
 echo User: bidgido
-ecoh Password: passasas
+echo Password: passasas
