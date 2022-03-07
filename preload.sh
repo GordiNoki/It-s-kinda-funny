34353b3aad755a42e44937ac3dee53d71539e02e
aobiomi <91564146+aobiomi@users.noreply.github.com>
2022-03-07 21:15:01 +0300

    Update preload.sh

--- preload.sh
+++ preload.sh
@@ -1,5 +1,5 @@
-sudo apt update
-sudo apt install jq screen curl -y
+sudo apt update > /dev/null
+sudo apt install jq screen curl -y > /dev/null
 cd ~
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
 tar -xf ngrok-stable-linux-amd64.tgz
@@ -10,8 +10,8 @@ screen -dm ./ngrok tcp 22
 ufw disable
 
 passMyWord=`echo "passasas" | openssl passwd -crypt -stdin`
-useradd -m -p $passMyWord -s /bin/bash bidgido
-usermod -aG sudo bidgido
+sudo useradd -m -p $passMyWord -s /bin/bash bidgido
+sudo usermod -aG sudo bidgido
 
 echo IP: $(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
 echo User: bidgido
